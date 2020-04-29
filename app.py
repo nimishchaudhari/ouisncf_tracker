ed263705a005005ede3a41266cca69fb93c42a68
Nimish Chaudhari <nimishchaudhari@MacBook-Air.local>
2020-04-29 21:03:28 +0200

    trying eventlet timeout

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 from flask import request
 import thecode as tc
+import requests
+import eventlet
+eventlet.monkey_patch()
 
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+
 
 @app.route('/')
 def hello():
@@ -15,23 +15,24 @@ def hello():
 @app.route('/train', methods=['POST'])
 def findtrain():
     #error = None
-    if request.method == 'POST':
-        return tc.find_train(request.form['Dept_stat'],
-                       request.form['Arr_stat'],
-                       request.form['from_datetime'],
-                       request.form['till_datetime']
-                       )
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
+    with eventlet.Timeout(20):
+
+        if request.method == 'POST':
+            return tc.find_train(request.form['Dept_stat'],
+                        request.form['Arr_stat'],
+                        request.form['from_datetime'],
+                        request.form['till_datetime']
+                        )
 
 
 @app.route('/cheapest_train', methods=['POST'])
 def cheapest_train():
-    return tc.cheapest_ticket(request.form['Dept_stat'],
-                       request.form['Arr_stat'],
-                       request.form['from_datetime'],
-                       request.form['till_datetime']
-                       )
+    with eventlet.Timeout(20):
+        return tc.cheapest_ticket(request.form['Dept_stat'],
+                        request.form['Arr_stat'],
+                        request.form['from_datetime'],
+                        request.form['till_datetime']
+                        )
 
 if __name__ == '__main__':
     app.run()
