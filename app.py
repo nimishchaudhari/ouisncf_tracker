121c870126789a8768daf8b0d0bd9cdc310c2f53
Nimish Chaudhari <nimishchaudhari@MacBook-Air.local>
2020-04-29 21:12:16 +0200

    failed, trying new stuff

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 from flask import request
 import thecode as tc
 import requests
-import eventlet
-eventlet.monkey_patch()
+
 
 
 
@@ -15,24 +14,21 @@ def hello():
 @app.route('/train', methods=['POST'])
 def findtrain():
     #error = None
-    with eventlet.Timeout(20):
-
-        if request.method == 'POST':
-            return tc.find_train(request.form['Dept_stat'],
-                        request.form['Arr_stat'],
-                        request.form['from_datetime'],
-                        request.form['till_datetime']
-                        )
+    if request.method == 'POST':
+        return tc.find_train(request.form['Dept_stat'],
+                    request.form['Arr_stat'],
+                    request.form['from_datetime'],
+                    request.form['till_datetime']
+                    )
 
 
 @app.route('/cheapest_train', methods=['POST'])
 def cheapest_train():
-    with eventlet.Timeout(20):
-        return tc.cheapest_ticket(request.form['Dept_stat'],
-                        request.form['Arr_stat'],
-                        request.form['from_datetime'],
-                        request.form['till_datetime']
-                        )
+    return tc.cheapest_ticket(request.form['Dept_stat'],
+                    request.form['Arr_stat'],
+                    request.form['from_datetime'],
+                    request.form['till_datetime']
+                    )
 
 if __name__ == '__main__':
     app.run()
